02dec7cb6b1f55a11c2a2f834d72054d26b9c395
mauriciomutte <mauriciomutte.mm@gmail.com>
2025-02-13 21:05:56 -0300

    feat(models): add Choice model question field as ForeignKey

--- polling/apps/core/models.py
+++ polling/apps/core/models.py
@@ -16,7 +16,9 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(
+        Question, related_name="choices", on_delete=models.CASCADE
+    )
     choice_text = models.CharField(max_length=200)
 
     def __str__(self):
