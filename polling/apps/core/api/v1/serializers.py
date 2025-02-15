0e289f123a94a4359cc0c91c3b8a01a10298f551
mauriciomutte <mauriciomutte.mm@gmail.com>
2025-02-15 13:33:21 -0300

    feat(serializers): add `VoteSerializer` with validations

--- polling/apps/core/api/v1/serializers.py
+++ polling/apps/core/api/v1/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 
-from polling.apps.core.models import Question
+from polling.apps.core.models import Choice, Question, Vote
 
 
 class QuestionSerializer(serializers.ModelSerializer):
     choices = serializers.SerializerMethodField()
 
     def get_choices(self, obj):
-        return obj.choices.values_list("choice_text", flat=True)
+        return obj.choices.values("id", "choice_text")
 
     class Meta:
         model = Question
@@ -21,3 +21,37 @@ class CreatePollSerializer(serializers.Serializer):
         child=serializers.CharField(),
         min_length=2,  # Ensure at least 2 choices are provided
     )
+
+
+class VoteSerializer(serializers.Serializer):
+    poll_id = serializers.UUIDField()
+    choice_id = serializers.UUIDField()
+    session_id = serializers.CharField(required=False, allow_null=True)
+
+    def validate(self, attrs):
+        try:
+            question = Question.objects.get(id=attrs["poll_id"])
+            choice = Choice.objects.get(id=attrs["choice_id"])
+
+            # Validate choice belongs to question
+            if choice.question_id != question.id:
+                raise serializers.ValidationError("Choice does not belong to this poll")
+
+            # Validate if user has already voted
+            if attrs["session_id"]:
+                prev_vote = Vote.objects.filter(
+                    question=question, session_id=attrs["session_id"]
+                )
+
+                if prev_vote.exists():
+                    raise serializers.ValidationError("You have already voted")
+
+            attrs["question"] = question
+            attrs["choice"] = choice
+
+            return attrs
+
+        except Question.DoesNotExist:
+            raise serializers.ValidationError("Poll not found")
+        except Choice.DoesNotExist:
+            raise serializers.ValidationError("Choice not found")
